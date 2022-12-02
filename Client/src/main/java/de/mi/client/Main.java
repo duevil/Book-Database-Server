18ce20649f97e1ee4213b8dde502445afc1ea391
Malte <maltekasolowsky@gmail.com>
2022-12-02 19:30:00 +0100

    Version 0.1.1: added basic packages and module-info.java files; updated build.gradle files, adding most libraries

---
+++ Client/src/main/java/de/mi/client/Main.java
@@ -0,0 +1,7 @@
+package de.mi.client;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: start client
+    }
+}
---
+++ Server/src/main/java/de/mi/server/Main.java
@@ -0,0 +1,7 @@
+package de.mi.server;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: start server
+    }
+}
